aac486eaa956e92bc0ad58cee0a9a013eb6e768f
Diego Vila <diego@velascommerce.com>
2022-11-21 20:42:57 -0400

    got podman and docker-compose working together

--- deno/deno-by-example/main.ts
+++ deno/deno-by-example/main.ts
@@ -1,7 +1,7 @@
-import deno_demo from "./deno_demo.ts";
-import lodash from "./lodash.ts";
-import logs from "./log.ts";
-import typescript from "./typescript/mod.ts";
+// import deno_demo from "./deno_demo.ts";
+// import lodash from "./lodash.ts";
+// import logs from "./log.ts";
+// import typescript from "./typescript/mod.ts";
 
 // deno upgrade : upgrade deno
 // Environment variables
@@ -9,10 +9,11 @@ import typescript from "./typescript/mod.ts";
 //    DENO_INSTALL_ROOT: where installed scripts are stored
 
 if (import.meta.main) {
-  await deno_demo();
-  lodash();
-  logs();
-  typescript();
+  console.log("Hello deno docker");
+  // await deno_demo();
+  // lodash();
+  // logs();
+  // typescript();
   // await demo();
   // lodash_demo();
   // log_demo();
