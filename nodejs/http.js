0f328de9d53bf39f2e32e4d42b4bb631ea580125
ruahman <dego_vila@yahoo.com>
2023-06-24 20:08:35 -0400

    started with http

--- nodejs/http.js
+++ nodejs/http.js
@@ -1,10 +1,35 @@
+const http = require("http");
+const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
 
-const http = require('http');
+var server = http.createServer(function (req, res) {
+  let parsedURL = url.parse(req.url, true);
+  let path = parsedURL.pathname;
+  let trimmedPath = path.replace(/^\/+|\/+$/g, "");
 
-var server = http.createServer(function(req,res){
-  res.end('hello world\n');
-})
+  let method = req.method.toLowerCase();
+  let queryStringObject = parsedURL.query;
+  let headers = req.headers;
 
-server.listen(3000, function(){
+  // payload comes in as stream, little bits at a time
+  let decoder = new StringDecoder("utf-8");
+  let buffer = "";
+  // data come in one chunk at a time
+  req.on("data", (data) => {
+    buffer += decoder.write(data);
+  });
+  req.on("end", () => {
+    buffer += decoder.end();
+    res.end("hello world\n");
+    console.log(
+      `request was received on this path: ${trimmedPath} with this method: ${method} and querystring:`,
+      queryStringObject
+    );
+    console.log("headers: ", headers);
+    console.log("payload: ", buffer);
+  });
+});
+
+server.listen(3000, function () {
   console.log("we are running server on port 3000");
-})
+});
