481735815e6872e001d00178c18de1c08f6852a1
ruahman <dego_vila@yahoo.com>
2023-09-24 20:35:21 -0400

    design patters
    got started with design patters

---
+++ typescript/conditional_types.ts
@@ -0,0 +1,26 @@
+// user terinary logic to determine the type of a variable
+
+// if T extends x then SomeType is A else SomeType is B
+// type SomeType = T extends X ? A : B;
+
+type SomeType = string;
+type MyConditionalType = SomeType extends string ? string : null;
+
+function someFunction<T>(value: T) {
+  type ConditiionalType2 = SomeType extends string
+    ? string
+    : SomeType extends number
+    ? number
+    : SomeType extends boolean
+    ? boolean
+    : null;
+
+  const someOtherFunction = <A extends ConditiionalType2>(someArg: A): A => {
+    const a = someArg;
+    return a;
+  };
+  return someOtherFunction;
+}
+
+const result = someFunction(true);
+const result2 = someFunction("string");
