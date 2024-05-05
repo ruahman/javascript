211db96742bd7eef883b11bb7ff527f36943a80e
Diego Vila <ruahman@gmail.com>
2024-05-05 14:17:22 -0400

    exhaustive type checking

---
+++ typescript/exhaustive_type_checking.test.ts
@@ -0,0 +1,6 @@
+import { test, expect } from "bun:test";
+import exhaustive_type_checking from "./exhaustive_type_checking";
+
+test("exhaustive_type_checking", () => {
+  exhaustive_type_checking(expect);
+});
