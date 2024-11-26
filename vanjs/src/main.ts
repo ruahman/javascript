bf304ab4032be3ef4834a0599552dd6846bc2884
Diego R Vila <dego_vila@yahoo.com>
2024-11-26 17:40:45 -0400

    refactor: refactor typescript

--- vanjs/src/main.ts
+++
@@ -1,6 +0,0 @@
-import van from "vanjs-core";
-const { p, div, h1 } = van.tags;
-
-const App = () => div(h1("Hello from VanJS!"), p("This is running with Vite."));
-
-van.add(document.getElementById("app")!, App());
