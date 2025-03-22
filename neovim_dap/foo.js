424a91217cecd610353e40989b43abd4a50cf167
Diego R Vila <dego_vila@yahoo.com>
2025-03-21 23:26:27 -0400

    dap

---
+++ neovim_dap/foo.js
@@ -0,0 +1,5 @@
+"use strict";
+function foo(arg) {
+    return arg + 1;
+}
+console.log("foo: ", foo(2));
---
+++ neovim_dap/foo.ts
@@ -0,0 +1,5 @@
+function foo(arg: number): number {
+	return arg + 1;
+}
+
+console.log("foo: ", foo(2));
