64d5f6b3251057e76cea0ddb7f8ccb623c9d0575
Diego R Vila <dego_vila@yahoo.com>
2025-03-27 00:12:36 -0400

    tsc --watch

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -3,7 +3,7 @@ import assert from "node:assert/strict";
 function index(): void {
   const test: number = 22;
   console.log(test);
-  console.log("hello world");
+  console.log("hello worldxxxx");
   console.log("From index()");
   assert.equal(test, 22);
 }
